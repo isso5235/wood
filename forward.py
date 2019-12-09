0be43333f68b179cd59f5798bc752afddef5002c
isso5235 <gakkouyou7841@gmail.com>
2019-12-09 17:10:42 +0900

    HMM

--- forward.py
+++ forward.py
@@ -1,8 +1,4 @@
 import numpy as np
-import math
-from decimal import *
-
-getcontext().prec = 100
 
 T = 1000    #記号列の長さ
 N = 2       #出力状態数
@@ -11,35 +7,25 @@ Pi_1 = np.array( [1.0, 0.0] )   #初期分布
 A_1  = np.array( [[0.2, 0.8], [0.8, 0.2]] ) #推移確率
 B_1  = np.array( [[0.8, 0.2], [0.2, 0.8]] ) #出力確率
 
-O_1 = [ np.random.choice( [0,1], p = [0.2, 0.8] ) for i in range(T) ]   #出力記号列
-O_2 = [ np.random.choice( [0,1], p = [0.2, 0.8] ) for i in range(T) ]   #出力記号列
+O = [ np.random.choice( [0,1], p = [0.2, 0.8] ) for i in range(T) ]   #出力記号列
 
 P_lambda = 0
 
 alpha = np.zeros((T, N))
+C = np.zeros((T))   #スケーリング用
 
 #初期値設定
-for j in range(N):
-    alpha[0, j] = Pi_1[j] * B_1[j, O_1[0]]
-    #print(alpha[0, j])
+alpha[0, :] = Pi_1[:] * B_1[:, O[0]]
+C[0] = alpha[0, :].sum()
+alpha[0, :] /= C[0]
 
 #繰り返し
 for t in range(T-1):
-    #print(B_1[j, O_1[t+1]])
-    for j in range(N):
-        Sum = 0
-        for i in range(N):
-            Sum += alpha[t, i] * A_1[i,j]
-            print(math.log(Sum))
-            #print(Decimal(alpha[t, i] * A_1[i,j]))
-        alpha[t+1, j] = Sum*B_1[j, O_1[t+1]]   
-        
+    alpha[t+1, :] = alpha[t, :].dot(A_1) * B_1[:, O[t+1]]
+    C[t+1] = alpha[t+1, :].sum()
+    alpha[t+1, :] /= C[t+1]
 
 #P(lambda)を計算
-for j in range(N):
-    P_lambda += alpha[T-1, j]
+P_lambda = -np.log(C).sum()
 
-
-#print(np.log(P_lambda))
-
-#np.dot(alpha[t-1, :], A)
+print(-P_lambda)
